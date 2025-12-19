bb6723bea57cac6fb2a6cdc0d2b39302f4a4e9f0
Maykon Granemann <maykondgranemann@gmail.com>
2025-12-18 22:18:31 -0300

    criando menu da aplicacao

---
+++ aula007/teste.py
@@ -0,0 +1,30 @@
+from time import sleep
+
+opcao = -1
+
+while opcao != 0:
+    print('='*20, 'Modulo Funcionario' ,'='*20, '\n')
+
+    print('\t1-Cadastrar')
+    print('\t2-Editar')
+    print('\t3-Listar')
+    print('\t4-Deletar')
+    print('\t0-Sair')
+
+    opcao = int(input('\n\tEscolha uma das opcoes:'))
+
+    match opcao:
+        case 0:
+            print('Saindo ...')
+        case 1:
+            print('Cadastrando ...')
+        case 2:
+            print('Editando ...')
+        case 3:
+            print('Listando ...')
+        case 4:
+            print('Deletando ...')
+        case _:
+            print('Opcao invalida')
+    
+    sleep(2)
